b029dac28c3c0b369a85ab2d25c68cf3b07dc3f1
Rory Hellier <rory.hellier@googlemail.com>
2020-05-22 18:17:43 +0100

    Added placeholder 'add as friend button to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @friend = @user.is_friend?(@current_user)
   end
 end
