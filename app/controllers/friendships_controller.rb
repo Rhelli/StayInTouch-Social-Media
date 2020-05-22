96251d1871457c941c1ca37c111d090239725ecb
Rory Hellier <rory.hellier@googlemail.com>
2020-05-22 18:48:49 +0100

    Created The Friendships Controller
     - Only an outline atm. Has new, create, update, destroy and strong params but all need testing
    
     - REMINDER: Continue working on the friendships controller. Friendships routes need configuring, update and destroy methods need filling in

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,29 @@
+class FriendshipsController < ApplicationController
+  
+  def new
+    @friendship = current_user.friendships.new
+  end
+
+  def create
+    @friendship = current_user.friendships.new(friendships_params)
+    if @friendship.save
+      flash.now[:success] = 'Your friend request has been sent.'
+    else
+      flash.now[:danger] = 'An error occurred. Please try again.'
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:friend)
+  end
+end
