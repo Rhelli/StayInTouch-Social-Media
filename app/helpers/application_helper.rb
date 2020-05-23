4413d6564134a73538a06e8f7ef079d1e4a0742e
Rory Hellier <rory.hellier@googlemail.com>
2020-05-23 16:33:27 +0100

    Added Gravatars
     - Added 'gravatar_for(user) method in the application helper'
     - Added gravatar_for call to the _suggestsed_friends partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,10 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name.titleize, class: "gravatar")
+  end
 end
