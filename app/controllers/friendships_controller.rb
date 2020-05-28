043aabc1e1a815e34ac59f03cc405b20070ec2cb
Rory Hellier <rory.hellier@googlemail.com>
2020-05-28 13:57:06 +0100

    Added functional 'add user' button the the user index page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -10,24 +10,47 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = Friendship.create_inverse_friendship(current_user, params[:friend_id])
+    return if current_user.id == params[user.id]
+    @user = User.find_by(params[:user_id])
+    @friendship = current_user.friendship.build(friend_id: params[:user_id])
     if @friendship.save
-      flash.now[:success] = "Friend Request Sent."
+      flash.now[:success] = 'Friend request sent.'
     else
-      flash[:danger] = "An Error Occurred. Please Try Again!"
+      flash[:danger] = 'An error has occurred, please try again!'
       respond_to do |format|
-        format.js { render inline: "location.reload()"}
+        format.js { render inline: 'location.reload();'}
       end
     end
   end
 
-  def update
+  def accept_request
+    @friendship = Friendship.find_by(user_id: params[:user_id], friend_id: current_user.id, confirmed: false)
+    # Returns unless the friendship is found
+    return unless @friendship
+    @friendship.status = true
+    if @friendship.save
+      flash.now[:success] = 'Friend request accepted.'
+    else
+      flash[:danger] = 'An error occurred, please try again!'
+      respond_to do |format|
+        format.js { render inline: 'location.reload();' }
+      end
+    end
+  end
 
+  def decline_request
+    @friendship = Friendship.find_by(friend_id: params[:user_id], friend_id: current_user.id, status: false)
+    # Return if no friendship is found
+    return unless @friendship
+    @friendship.destroy
+    flash[:success] = 'Friend request declined.'
+    respond_to do |format|
+      format.js { render inline: 'location.reload();' }
+    end
   end
 
   def destroy
-    Friendship.destroy_inverse_friendship(current_user, params[:friend_id])
-    redirect_to(request.referer)
+
   end
 
   private
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,18 +5,4 @@ class Friendship < ApplicationRecord
   scope :confirmed_friends, -> { where('status = ?', true) }
   scope :unconfirmed_friends, -> { where('status = ?', false) }
 
-  # Creates new friendship for friend as well as user when created
-  def self.create_inverse_friendship(user_id, friend_id)
-    user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
-    friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
-    [user_friendship, friend_friendship]
-  end
-
-  # Destroys inverse friendship
-  def self.destroy_inverse_friendship(user_id, friend_id)
-    d_user_friendship = Friendship.find_by(user_id: user_id, friend_id: friend_id)
-    d_friend_friendship = Friendship.find_by(user_id: friend_id, friend_id: user_id)
-    d_user_friendship.destroy
-    d_friend_friendship.destroy
-  end
 end
