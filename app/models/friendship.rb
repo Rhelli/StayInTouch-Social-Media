0a9fd92fa0784c9ab7184f848184ed543727129b
Rory Hellier <rory.hellier@googlemail.com>
2020-06-08 17:55:46 +0100

    Resolved Friendship Duplication Issues
     - Added validation for unqiueness for friend, preventing the duplication of one specific invite from user to friend
     - Added method prevent_duplicates to lookup if the user already had a request from the friend and reject if so
     - Added method inverse_exists? to lookup if the friend had recieved any requests from the user to allow for the creation of inverse relationships
     - Encapsulated these methods in a validation callback

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+ 
+  validate :not_inviting_self
+  validate :prevent_duplicates, if: :inverse_exists?
+  validates :friend, uniqueness: { scope: :user, message: 'Invitation already sent.' }
 
   def accepted
     update(confirmed: true)
@@ -8,5 +12,19 @@ class Friendship < ApplicationRecord
 
   def declined
     update(confirmed: false)
+  end
+
+  protected
+
+  def not_inviting_self
+    errors.add(:friend, 'You cannot add yourself!') if friend_id == user_id
+  end
+
+  def prevent_duplicates
+    errors.add(:friend, 'This user has already sent you a friend request!') if User.invited_requests(user).include?(friend)
+  end
+
+  def inverse_exists?
+    User.pending_requests(friend).include?(user)
   end
 end
