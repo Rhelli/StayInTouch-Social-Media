5d5ab04af45911087338ffd5e574e298910ab0a7
Rory Hellier <rory.hellier@googlemail.com>
2020-06-10 19:44:40 +0100

    More Timeline Styling
     - Styled individual posts more. Edited comments section
     - Enlarged the gravatar and placed it absolute so it hovers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,9 @@ module ApplicationHelper
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
-      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
+      link_to('Unlike', post_like_path(id: like.id, post_id: post.id), method: :delete)
     else
-      link_to('Like!', post_likes_path(post_id: post.id), method: :post)
+      link_to('Like', post_likes_path(post_id: post.id), method: :post)
     end
   end
 
